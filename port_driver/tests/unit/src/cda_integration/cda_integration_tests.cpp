44c455986d5398466677324348ccbef69512bb07
Sushant Sharma <shsush@amazon.com>
2022-05-02 15:25:49 -0700

    Add tests

--- port_driver/tests/unit/src/cda_integration/cda_integration_tests.cpp
+++ port_driver/tests/unit/src/cda_integration/cda_integration_tests.cpp
@@ -11,13 +11,28 @@ namespace port_driver {
     namespace tests {
         namespace unit {
             class CDAIntegrationTester : public ::testing::Test {
+            private:
+                static const std::string TEST_CLIENT_ID;
+                static const std::string TEST_CLIENT_PEM;
+
             protected:
                 CDAIntegrationTester() {
                 }
             };
 
-            TEST_F(CDAIntegrationTester, CDAIntegrationInitTest) {
-                EXPECT_EQ(7 * 6, 42);
+            const std::string CDAIntegrationTester::TEST_CLIENT_ID = "test_client_id";
+            const std::string CDAIntegrationTester::TEST_CLIENT_PEM = "test_client_pem";
+
+            TEST_F(CDAIntegrationTester, CDAIntegrationInitAndCloseTest) {
+                CDA_INTEGRATION_HANDLE* handle = cda_integration_init();
+                EXPECT_NE(nullptr, handle);
+                EXPECT_TRUE(cda_integration_close(handle));
+            }
+
+            TEST_F(CDAIntegrationTester, CDAIntegrationOnClientConnectTest) {
+                CDA_INTEGRATION_HANDLE* handle = cda_integration_init();
+                EXPECT_TRUE(on_client_connect(handle, test_client_id.c_str(), test_client_pem.c_str()));
+                EXPECT_TRUE(cda_integration_close(handle));
             }
         }
     }
