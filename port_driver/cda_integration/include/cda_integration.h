1a049329a4ec0c2506fc4bdd2ecc0a287c34a00c
Michael Dombrowski <mdombro@amazon.com>
2022-05-03 17:45:24 -0400

    build: switch port driver to C++ with clang best practices (#17)

--- port_driver/cda_integration/include/cda_integration.h
+++ port_driver/cda_integration/include/cda_integration.h
@@ -5,33 +5,22 @@
 
 #pragma once
 
-#include <stdbool.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /*
  * @brief Handle to client device auth integration
  */
-typedef void* CDA_INTEGRATION_HANDLE;
+typedef void *CDA_INTEGRATION_HANDLE;
 
-CDA_INTEGRATION_HANDLE* cda_integration_init();
+CDA_INTEGRATION_HANDLE *cda_integration_init();
 
-bool on_client_connect(CDA_INTEGRATION_HANDLE* handle, const char* clientId, const char* pem);
+bool on_client_connect(CDA_INTEGRATION_HANDLE *handle, const char *clientId, const char *pem);
 
-bool on_client_connected(CDA_INTEGRATION_HANDLE* handle, const char* clientId, const char* pem);
+bool on_client_connected(CDA_INTEGRATION_HANDLE *handle, const char *clientId, const char *pem);
 
-bool on_client_disconnected(CDA_INTEGRATION_HANDLE* handle, const char* clientId, const char* pem);
+bool on_client_disconnected(CDA_INTEGRATION_HANDLE *handle, const char *clientId, const char *pem);
 
-bool on_client_authenticate(CDA_INTEGRATION_HANDLE* handle, const char* clientId, const char* pem);
+bool on_client_authenticate(CDA_INTEGRATION_HANDLE *handle, const char *clientId, const char *pem);
 
-bool on_check_acl(CDA_INTEGRATION_HANDLE* handle, const char* clientId, const char* pem, const char* topic,
-    const char* action);
+bool on_check_acl(CDA_INTEGRATION_HANDLE *handle, const char *clientId, const char *pem,
+                  const char *topic, const char *action);
 
-bool cda_integration_close(CDA_INTEGRATION_HANDLE* handle);
-
-#ifdef __cplusplus
-}
-#endif
-
+bool cda_integration_close(CDA_INTEGRATION_HANDLE *handle);
