e2d5b2bfa136c4f24c48357d27895994f10a518a
Ashay Vipinkumar <avipinku@amazon.com>
2022-05-05 15:49:33 -0700

    feat: retrieve server certs from cda

--- port_driver/cda_integration/lib/cda_integration.cpp
+++ port_driver/cda_integration/lib/cda_integration.cpp
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <filesystem>
+#include <fstream>
+#include <string>
 #include <aws/crt/Api.h>
 #include <aws/crt/io/HostResolver.h>
 #include <aws/greengrass/GreengrassCoreIpcClient.h>
@@ -11,8 +14,19 @@
 
 #include "cda_integration.h"
 
+using namespace Aws::Crt;
+using namespace Aws::Greengrass;
+using namespace std;
+
+const string EMQX_KEY_PATH = "/etc/greengrass_certs/greengrass_emqx.key";
+const string EMQX_PEM_PATH = "/etc/greengrass_certs/greengrass_emqx.pem";
+const string EMQX_CA_PATH = "/etc/greengrass_certs/greengrass_ca.pem";
+
 class ClientDeviceAuthIntegration {
-  public:
+private:
+    unique_ptr<GreengrassCoreIpcClient> ipcClient;
+    unique_ptr<ApiHandle> apiHandle;
+public:
     ClientDeviceAuthIntegration();
 
     bool close() const;
@@ -28,9 +42,126 @@ class ClientDeviceAuthIntegration {
     bool on_check_acl(const char *clientId, const char *pem, const char *topic, const char *action);
 
     bool verify_client_certificate(const char *certPem);
+
+    int retrieveCertsFromCda();
 };
 
-ClientDeviceAuthIntegration::ClientDeviceAuthIntegration() {}
+/*
+ * Inheriting from ConnectionLifecycleHandler allows us to define callbacks that are
+ * called upon when connection lifecycle events occur.
+ */
+class TestConnectionLifecycleHandler : public ConnectionLifecycleHandler{
+    public:
+        TestConnectionLifecycleHandler() {}
+        void OnConnectCallback() override { fprintf(stdout, "Connected to Greengrass Core\n"); }
+        void OnDisconnectCallback(RpcError status) override{
+            if (!status){
+                fprintf(stdout, "Disconnected from Greengrass Core with error: %s\n", status.StatusToString().c_str());
+                exit(-1);
+            }
+        }
+        bool OnErrorCallback(RpcError status) override {
+            fprintf(
+                stdout,
+                "Processing messages from the Greengrass Core resulted in error: %s\n",
+                status.StatusToString().c_str());
+            return true;
+        }
+};
+
+int ClientDeviceAuthIntegration::retrieveCertsFromCda(){
+
+    class CertificateUpdatesStreamHandler : public SubscribeToCertificateUpdatesStreamHandler {
+        void OnStreamEvent(CertificateUpdateEvent *response) override {
+            fprintf(stdout, "Retrieving all certs...\n");
+            auto certUpdate = response->GetCertificateUpdate();
+            auto privateKey = certUpdate->GetPrivateKey();
+            auto cert = certUpdate->GetCertificate();
+            auto allCas = certUpdate->GetCaCertificates();
+            fprintf(stdout, "Retrieved all certs from response...\n");
+
+            auto cwd = std::filesystem::current_path().string();
+            fprintf(stdout, "Current working directory is: %s \n", cwd.c_str());
+            ofstream out_key(cwd + EMQX_KEY_PATH);
+            out_key << privateKey.value().c_str();
+            out_key.close();
+            ofstream out_pem(cwd + EMQX_PEM_PATH);
+            out_pem << cert.value().c_str();
+            out_pem.close();
+            ofstream out_ca(cwd + EMQX_CA_PATH);
+            out_ca << allCas.value().front().c_str();
+            out_ca.close();
+            fprintf(stdout, "Wrote all certs!\n");
+        }
+
+        bool OnStreamError(OperationError *error) override {
+            fprintf(stderr, "OnStream error %s\n", error->GetMessage().value().c_str());
+            return false; // Return true to close stream, false to keep stream open.
+        }
+
+        void OnStreamClosed() override {
+            fprintf(stdout, "Stream closed\n");
+            // Handle close.
+        }
+    };
+
+    SubscribeToCertificateUpdatesRequest request;
+    CertificateOptions* options = new CertificateOptions();
+    options->SetCertificateType(CERTIFICATE_TYPE_SERVER);
+    request.SetCertificateOptions(*options);
+
+    shared_ptr<CertificateUpdatesStreamHandler> streamHandler(new CertificateUpdatesStreamHandler());
+    auto operation = ipcClient->NewSubscribeToCertificateUpdates(streamHandler);
+    auto activate = operation->Activate(request, nullptr);
+    activate.wait();
+
+    auto responseFuture = operation->GetResult();
+    if (responseFuture.wait_for(std::chrono::seconds(10)) == std::future_status::timeout) {
+        std::cerr << "Operation timed out while waiting for response from Greengrass Core." << std::endl;
+        exit(-1);
+    }
+    auto response = responseFuture.get();
+    fprintf(stdout, "Received response from CDA...\n");
+    if (!response) {
+        fprintf(stderr, "Empty response\n");
+        // Handle error.
+        auto errorType = response.GetResultType();
+        fprintf(stdout, "Subscribe error %d\n", errorType);
+        if (errorType == OPERATION_ERROR) {
+            auto *error = response.GetOperationError();
+            fprintf(stderr, "Cert subscribe response error %s\n", error->GetMessage().value().c_str());
+        } else {
+            // Handle RPC error.
+        }
+        return -1;
+    }
+    fprintf(stdout, "Done subscribing\n");
+    return 0;
+}
+
+ClientDeviceAuthIntegration::ClientDeviceAuthIntegration() {
+    fprintf(stdout, "Attempting to initialize Greengrass IPC client...\n" );
+    apiHandle = unique_ptr<ApiHandle>(new ApiHandle{g_allocator});
+    if (apiHandle->GetOrCreateStaticDefaultClientBootstrap()->LastError() != AWS_ERROR_SUCCESS){
+        fprintf(
+            stderr,
+            "ClientBootstrap failed with error %s\n",
+            ErrorDebugString(apiHandle->GetOrCreateStaticDefaultClientBootstrap()->LastError()));
+        exit(-1);
+    }
+    ipcClient = unique_ptr<GreengrassCoreIpcClient>(new GreengrassCoreIpcClient(*apiHandle->GetOrCreateStaticDefaultClientBootstrap()));
+
+    TestConnectionLifecycleHandler lifecycleHandler;
+    auto connectionStatus = ipcClient->Connect(lifecycleHandler).get();
+    if (!connectionStatus)
+    {
+        fprintf(stderr, "Failed to establish connection with error %s\n", connectionStatus.StatusToString().c_str());
+        exit(-1);
+    }
+    fprintf(stdout, "Greengrass IPC Client created and connected successfully!\n" );
+    int subscribe_return = ClientDeviceAuthIntegration::retrieveCertsFromCda();
+    std::cout << "Retrieved certs from CDA with status : " << subscribe_return << std::endl;
+}
 
 bool ClientDeviceAuthIntegration::close() const { return true; }
 
--- src/aws_greengrass_emqx_certs.erl
+++ src/aws_greengrass_emqx_certs.erl
@@ -17,76 +17,25 @@
 load() ->
   loadAllServerCerts().
 
-%% Load in greengrass_ca.pem, greengrass_emqx.key, greengrass_emqx.pem, and reload the PEM cache
+%% Check for greengrass_ca.pem, greengrass_emqx.key, greengrass_emqx.pem, and reload the PEM cache
 loadAllServerCerts() ->
-  CaCert = retrieveServerCert(?CA_FILE),
-  writeServerCert(?CA_FILE, CaCert),
-
-  Key = retrieveServerCert(?KEY_FILE),
-  writeServerCert(?KEY_FILE, Key),
-
-  Pem = retrieveServerCert(?PEM_FILE),
-  writeServerCert(?PEM_FILE, Pem),
-
-  logger:info("Successfully wrote all server certs to ~p", [?GG_CERT_REL_DIR]),
+  checkServerCert(?CA_FILE),
+  checkServerCert(?KEY_FILE),
+  checkServerCert(?PEM_FILE),
+  logger:info("Validated server certs in ~p", [?GG_CERT_REL_DIR]),
   cleanPemCache().
 
-% Temporary function that needs to be replaced with CDA retrieval
-% etc/emqx.conf must be configured to accept the files with mTLS
-% Retrieves hardcoded certs from the following locations and rewrites them to the same dir without a .bak extension
-% 'etc/greengrass_certs/greengrass_ca.pem.bak'
-% 'etc/greengrass_certs/greengrass_emqx.pem.bak'
-% 'etc/greengrass_certs/greengrass_emqx.key.bak'
-retrieveServerCert(FileName) ->
-  BakFileName = FileName ++ ".bak",
-  logger:debug("Retrieving cert from file ~p...", [BakFileName]),
-
-  FileExists = filelib:is_regular(BakFileName),
-  if
-    FileExists ->
-      logger:debug("Found cert file ~p", [BakFileName]);
-    true ->
-      logger:error("Cert file ~p does not exist!", [BakFileName]),
-      exit("Cert file does not exist at startup: ", [BakFileName])
-  end,
-
-  RetrievedData = case file:read_file(BakFileName) of
-                    {ok, Data} -> Data;
-                    {error, Reason} ->
-                      logger:error("Error reading from file ~p with message ~p and reason ~p", [BakFileName, error, Reason]),
-                      exit("Failed to read file: ", [BakFileName])
-                  end,
-  logger:debug("Read in cert file ~p", [BakFileName]),
-
-  IsBlank = string:is_empty(string:trim(RetrievedData)),
-  if
-    IsBlank ->
-      logger:error("Read cert file ~p is empty!", [BakFileName]),
-      exit("Found empty cert file during startup: ", [BakFileName]);
-    true ->
-      logger:debug("Validated cert file is not empty")
-  end,
-  RetrievedData.
-
 % Write server cert Data to specified location FileName
-writeServerCert(FileName, Data) ->
+checkServerCert(FileName) ->
   FileExists = filelib:is_regular(FileName),
   if
     FileExists ->
       %Until we have proper cert rotation in place, just exit
-      logger:error("Cert file ~p already exists!", [FileName]),
-      exit("Found existing cert file during startup!: ", [FileName]);
+      logger:error("Cert file ~p already exists!", [FileName]);
+%%      exit("Found existing cert file during startup!: ", [FileName]);
     true ->
-      logger:debug("Writing cert to file ~p...", [FileName])
-  end,
-
-  case file:write_file(FileName, Data) of
-    ok -> ok;
-    {error, Reason} ->
-      logger:error("Error writing to file ~p with message ~p and reason ~p", [FileName, error, Reason]),
-      exit("Failed to write to file: ", [FileName])
-  end,
-  logger:debug("Wrote to file ~p", [FileName]).
+      logger:warn("File does not exist yet!...", [FileName])
+  end.
 
 % Clean EMQX Pem Cache
 cleanPemCache() ->
@@ -94,6 +43,6 @@ cleanPemCache() ->
     ok = emqx_mgmt:clean_pem_cache()
   catch
     error:{badmatch, ok} -> logger:error("Failed to clean PEM cache!"),
-      exit("Failed to clean PEM cache!")
+    exit("Failed to clean PEM cache!")
   end,
   logger:info("Finished cleaning pem cache!").
