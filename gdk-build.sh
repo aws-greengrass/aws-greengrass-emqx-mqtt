f12b3f9e5751986b39eaaf4df9750f3fe98ae42c
Joseph Cosentino <jcosentino11@gmail.com>
2022-06-13 11:36:44 -0400

    chore: prevent build from running in echo command (#68)

--- gdk-build.sh
+++ gdk-build.sh
@@ -6,7 +6,7 @@ COMPONENT_NAME='aws.greengrass.clientdevices.mqtt.Emqx'
 VERSION='NEXT_PATCH'
 
 # Skip build as it is expensive. Just log instead. For now.
-echo "To build, first run `python3 -u -m bin`
+echo "To build, first run \"python3 -u -m bin\""
 #python3 -u -m bin
 
 mkdir -p greengrass-build/artifacts/${COMPONENT_NAME}/${VERSION}
