c9883fac0fc68cc18bcc2dc0513eee0fc31dfa55
Joseph Cosentino <joecose@amazon.com>
2022-06-14 12:47:14 -0400

    chore: replace loaded_plugins file

--- bin/package.py
+++ bin/package.py
@@ -2,24 +2,14 @@
 #  SPDX-License-Identifier: Apache-2.0
 import io
 import os
-import re
 import sys
 import tempfile
 import zipfile
 
 import patch as pypatch
 
-# plugins to disable in data/loaded_plugins
-disabled_emqx_plugins = [
-    "emqx_management"
-]
-# matches an entry in data/loaded_plugins
-# group 0: whole entry
-# group 1: plugin name
-# group 2: enabled bool
-loaded_plugin_pattern = r'{\s*(.*)\s*,\s*(true|false)\s*}\.'
 
-def update_zip(zipname, updates, add={}):
+def update_zip(zipname, updates, add):
     tmpfd, tmpname = tempfile.mkstemp(dir=os.path.dirname(zipname))
     os.close(tmpfd)
 
@@ -52,32 +42,12 @@ def patch(original, patch_file):
     return out.getvalue().decode()
 
 
-def append(original, addition):
-    return original.decode('utf-8') + addition
+def overwrite(replacement_file):
+    with open(replacement_file, 'r') as f:
+        return f.read()
 
 
 def do_patch(zip_path, erts_version="11.0", add=None):
-
-    def disable_plugins(plugins_to_disable=[]):
-        if not plugins_to_disable:
-            return
-
-        def _replace_plugin_entry(match):
-            if match.group(1) not in plugins_to_disable:
-                # no replacement
-                return match.group(0)
-            return f'{{{match.group(1)}, false}}.'
-
-        def _disable_plugins(contents):
-            return re.sub(loaded_plugin_pattern, _replace_plugin_entry, contents.decode('utf-8'))
-
-        update_zip(
-            zipname=zip_path,
-            updates={
-                "emqx/data/loaded_plugins": _disable_plugins
-            }
-        )
-
     # ini file is only for windows, but we'll just throw it in no matter what. Use \r\n for windows line endings
     with open("build/erl.ini", "w") as erl_ini:
         erl_ini.writelines(["[erlang]\r\n",
@@ -94,10 +64,8 @@ def do_patch(zip_path, erts_version="11.0", add=None):
         "emqx/bin/emqx.cmd": lambda o: patch(o, "patches/emqx.cmd.diff"),
         "emqx/bin/emqx": lambda o: patch(o, "patches/emqx.diff"),
         "emqx/bin/emqx_ctl.cmd": lambda o: patch(o, "patches/emqx_ctl.diff"),
-        "emqx/data/loaded_plugins": lambda o: append(o, "{aws_greengrass_emqx_auth, true}.\n"),
+        "emqx/data/loaded_plugins": lambda o: overwrite("patches/loaded_plugins"),
     }, add=add)
-
-    disable_plugins(plugins_to_disable=disabled_emqx_plugins)
 
 
 if __name__ == "__main__":
