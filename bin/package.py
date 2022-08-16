5c429ed6f79282b0efa8fb8d23dd8e1929a849b6
Joseph Cosentino <joecose@amazon.com>
2022-08-16 11:36:54 -0700

    fix: make emqx run in background again during startup

--- bin/package.py
+++ bin/package.py
@@ -51,6 +51,7 @@ def do_patch(zip_path, add=None):
         zipname=zip_path,
         updates={
             "emqx/bin/emqx": lambda o: patch(o, "patches/emqx.diff"),
+            "emqx/bin/emqx.cmd": lambda o: patch(o, "patches/emqx.cmd.diff"),
             "emqx/bin/emqx_ctl.cmd": lambda o: patch(o, "patches/emqx_ctl.diff")
         },
         add=add
