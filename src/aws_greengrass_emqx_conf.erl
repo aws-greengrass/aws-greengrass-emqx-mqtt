2bc236305c5f82016c9086cafe76e729ed5da30a
Joseph Cosentino <joecose@amazon.com>
2022-08-31 14:33:06 -0700

    refactor: define properties in plugin

--- src/aws_greengrass_emqx_conf.erl
+++ src/aws_greengrass_emqx_conf.erl
@@ -5,17 +5,17 @@
 
 -module(aws_greengrass_emqx_conf).
 
--export([auth_enabled/0]).
+-export([greengrass_authorization_mode/0, greengrass_broker_server_certificate_mode/0]).
 
--define(CONF_AUTH_ENABLED, greengrass_auth_enabled).
--define(DEFAULT_AUTH_ENABLED, true).
+-type(greengrass_authorization_mode() :: enabled | enabled_bypass_on_failure | bypass).
+-type(greengrass_broker_server_certificate_mode() :: enabled | disabled).
 
-%% Check if greengrass authN and authZ are enabled.
--spec(auth_enabled() -> boolean()).
-auth_enabled() ->
-  case emqx:get_env(?CONF_AUTH_ENABLED) of
-    undefined -> ?DEFAULT_AUTH_ENABLED;
-    true -> true;
-    "true" -> true;
-    _ -> false
-  end.
+-export_type([greengrass_authorization_mode/0, greengrass_broker_server_certificate_mode/0]).
+
+-spec(greengrass_authorization_mode() -> greengrass_authorization_mode()).
+greengrass_authorization_mode() ->
+  application:get_env(aws_greengrass_emqx_auth, greengrass_authorization_mode, enabled).
+
+-spec(greengrass_broker_server_certificate_mode() -> greengrass_broker_server_certificate_mode()).
+greengrass_broker_server_certificate_mode() ->
+  application:get_env(aws_greengrass_emqx_auth, greengrass_broker_server_certificate_mode, enabled).
