3954dbf22f809ee71a43764bada54bb3798093c2
Michael Dombrowski <mdombro@amazon.com>
2022-09-06 13:42:22 -0400

    refactor(logging): move common log level parsing to a header (#97)

---
+++ port_driver/common.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "common.h"
+
+aws_log_level crtStringToLogLevel(const std::string &level) {
+    // Crt defined log levels
+    if (level == "none") {
+        return AWS_LL_NONE;
+    }
+    if (level == "trace") {
+        return AWS_LL_TRACE;
+    }
+    if (level == "debug") {
+        return AWS_LL_DEBUG;
+    }
+    if (level == "info") {
+        return AWS_LL_INFO;
+    }
+    if (level == "warn") {
+        return AWS_LL_WARN;
+    }
+    if (level == "error") {
+        return AWS_LL_ERROR;
+    }
+    if (level == "fatal") {
+        return AWS_LL_FATAL;
+    }
+    // Default to warn
+    return AWS_LL_WARN;
+}
+
+aws_log_level erlangStringToLogLevel(const std::string &level) {
+    // Erlang log levels
+    // debug, info, notice, warning, error, critical, alert, emergency
+    if (level == "debug") {
+        return AWS_LL_TRACE;
+    }
+    if (level == "info") {
+        return AWS_LL_DEBUG;
+    }
+    if (level == "notice") {
+        return AWS_LL_INFO;
+    }
+    if (level == "warning") {
+        return AWS_LL_WARN;
+    }
+    if (level == "error") {
+        return AWS_LL_ERROR;
+    }
+    if (level == "emergency") {
+        return AWS_LL_FATAL;
+    }
+    // Default to warn
+    return AWS_LL_WARN;
+}
