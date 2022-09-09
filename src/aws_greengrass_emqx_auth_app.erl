daf4220f4a7157e639ec7f4f83e19fc647632c04
Joseph Cosentino <jcosentino11@gmail.com>
2022-09-09 16:50:07 +0000

    feat: use greengrass_authorization_mode feature flag  (#96)
    This commit implements the usage of greengrass_authorization_mode
    flag.  It controls whether our emqx auth hooks operate as normal,
    or can be bypassed.

--- src/aws_greengrass_emqx_auth_app.erl
+++ src/aws_greengrass_emqx_auth_app.erl
@@ -9,29 +9,31 @@
 
 -emqx_plugin(?MODULE).
 
--import(port_driver_integration, [start/0, stop/0]).
--import(tls_custom_certificate_verification, [enable/0]).
-
--export([start/2
-  , stop/1
-]).
+-export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
   {ok, Sup} = aws_greengrass_emqx_auth_sup:start_link(),
   port_driver_integration:start(),
-  case tls_custom_certificate_verification:enable() of
-    ok -> ok;
-    nossl ->
-      ErrorString = "Could not find active SSL listener",
-      throw({error, ErrorString});
-    {error, Reason} ->
-      ErrorString = io_lib:format("Failed to enable SSL custom certificate verification. Error: ~s",
-        [Reason]),
-      throw({error, ErrorString})
-  end,
+  enable_cert_verification(),
   aws_greengrass_emqx_certs:load(),
   aws_greengrass_emqx_auth:load(application:get_all_env()),
   {ok, Sup}.
+
+enable_cert_verification() ->
+  case aws_greengrass_emqx_conf:greengrass_authorization_mode() of
+    bypass -> ok;
+    _ ->
+      case tls_custom_certificate_verification:enable() of
+        ok -> ok;
+        nossl ->
+          ErrorString = "Could not find active SSL listener",
+          throw({error, ErrorString});
+        {error, Reason} ->
+          ErrorString = io_lib:format("Failed to enable SSL custom certificate verification. Error: ~s",
+            [Reason]),
+          throw({error, ErrorString})
+      end
+  end.
 
 stop(_State) ->
   aws_greengrass_emqx_auth:unload(),
