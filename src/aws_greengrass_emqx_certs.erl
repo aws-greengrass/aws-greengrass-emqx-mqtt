514e20c4f16539f28792ce4ff8dccbac2b8e117e
Michael Dombrowski <mdombro@amazon.com>
2022-09-22 10:09:26 -0400

    refactor: rename auth plugin options

--- src/aws_greengrass_emqx_certs.erl
+++ src/aws_greengrass_emqx_certs.erl
@@ -8,15 +8,15 @@
 -export([load/0]).
 
 load() ->
-  case aws_greengrass_emqx_conf:greengrass_broker_server_certificate_mode() of
-    enabled ->
+  case aws_greengrass_emqx_conf:use_greengrass_managed_certificates() of
+    true ->
       port_driver_integration:register_fun(certificate_update, fun cleanPemCache/0),
       %% Subscribe to certificate updates. On update,
       %% the certificate and its private key are written
       %% to the EMQX component work directory. The EMQX
       %% ssl listener is configured to read these files.
       port_driver_integration:request_certificates();
-    disabled ->
+    false ->
       ok
   end.
 
