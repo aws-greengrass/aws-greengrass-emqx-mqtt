6674a5168a6b3773fb2aae3be13763173fe8656e
Joseph Cosentino <joecose@amazon.com>
2023-06-05 15:41:48 -0700

    refactor: move config key name to separate file (#130)

---
+++ port_driver/include/config.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <string>
+
+namespace aws::greengrass::emqx {
+static const std::string localOverrideNamespace = "localOverride";
+}
