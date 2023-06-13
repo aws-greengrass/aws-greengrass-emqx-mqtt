eb3e10e32b0026e61a0ec63803304541f594209d
Joseph Cosentino <joecose@amazon.com>
2023-06-13 08:29:01 +0000

    feat: update plugin configuration on change

--- aws_greengrass_emqx_auth/src/aws_greengrass_emqx_conf.erl
+++ aws_greengrass_emqx_auth/src/aws_greengrass_emqx_conf.erl
@@ -6,17 +6,17 @@
 -module(aws_greengrass_emqx_conf).
 
 -export([auth_mode/0, use_greengrass_managed_certificates/0]).
--export([load/0]).
 
--export([update_configuration_from_ipc/0]).
+-export([start/0, stop/0]).
+-export([listen_for_update_requests/1, request_update/0]).
 
 -type(auth_mode() :: enabled | bypass_on_failure | bypass).
 -type(use_greengrass_managed_certificates() :: true | false).
 
 -export_type([auth_mode/0, use_greengrass_managed_certificates/0]).
 
--define(CONF_FILE_NAME, "aws_greengrass_emqx_auth.hocon").
 -define(ENV_APP, aws_greengrass_emqx_auth).
+-define(UPDATE_PROC, greengrass_config_update_listener).
 
 %% config keys
 -define(KEY_AUTH_MODE, auth_mode).
@@ -43,56 +43,42 @@ use_greengrass_managed_certificates() ->
 %% Config Loading
 %%--------------------------------------------------------------------
 
-%% Read plugin configuration from file and load it into the application environment.
-%% The config file is expected to reside in the plugin's priv dir, and the file is
-%% in hocon format.
--spec(load() -> ok | {error, any()}).
-load() ->
-  case read_config() of
-    {error, _} = Err -> Err;
-    Conf -> set_env(Conf)
+start() ->
+  spawn(?MODULE, listen_for_update_requests, [self()]),
+  receive
+    initialized -> ok
   end.
 
--spec(read_config() -> hocon:config() | {error, any()}).
-read_config() ->
-  case code:priv_dir(?ENV_APP) of
-    {error, Err} -> {error, {plugin_dir_not_found, Err}};
-    PluginConfDir ->
-      ConfFile = filename:join([PluginConfDir, ?CONF_FILE_NAME]),
-      read_config(ConfFile)
-  end.
-
--spec(read_config(file:filename()) -> hocon:config() | {error, any()}).
-read_config(ConfFile) ->
-  Conf = case hocon:load(ConfFile) of
-    {ok, C} -> C;
-    {error, Err} -> {error, {unable_to_read_config, Err}}
-  end,
-  try
-    {_, CheckedConf} =
-      hocon_tconf:map_translate(aws_greengrass_emqx_auth_schema, Conf, #{return_plain => true, format => map}),
-    emqx_map_lib:unsafe_atom_key_map(CheckedConf)
-  catch
-    throw:E:ST ->
-      {error, {config_validation, E, ST}}
-  end.
-
--spec(set_env(hocon:config()) -> ok).
-set_env(Conf) ->
-  logger:debug("Plugin Configuration: ~p", [Conf]),
-  %% TODO find a more dynamic way
-  RootConfig = maps:get(aws_greengrass_emqx_auth, Conf),
-  AuthMode = maps:get(?KEY_AUTH_MODE, RootConfig, ?DEFAULT_AUTH_MODE),
-  application:set_env(?ENV_APP, ?KEY_AUTH_MODE, AuthMode),
-  UseGreengrassManagedCertificates = maps:get(?KEY_USE_GREENGRASS_MANAGED_CERTIFICATES, RootConfig, ?DEFAULT_USE_GREENGRASS_MANAGED_CERTIFICATES),
-  application:set_env(?ENV_APP, ?KEY_USE_GREENGRASS_MANAGED_CERTIFICATES, UseGreengrassManagedCertificates).
+stop() ->
+  ?UPDATE_PROC ! stop.
 
 %% Retrieve Greengrass configuration via GetConfiguration IPC call,
 %% and update emqx override configuration with the result.
+request_update() ->
+  ?UPDATE_PROC ! update.
+
+listen_for_update_requests(Caller) ->
+  register(?UPDATE_PROC, self()),
+  Caller ! initialized,
+  do_listen_for_update_requests().
+
+do_listen_for_update_requests() ->
+  receive
+    update ->
+      logger:info("Config update request received"),
+      update_configuration_from_ipc(),
+      do_listen_for_update_requests();
+    stop -> ok
+  end.
+
 update_configuration_from_ipc() ->
   case port_driver_integration:get_configuration() of
-    {ok, Conf} -> update_configuration(Conf);
-    {error, Err} -> logger:error("Unable to get configuration, err=~p", [Err])
+  {ok, Conf} ->
+    update_configuration(Conf),
+    ok;
+  {error, Err} = Error ->
+    logger:error("Unable to get configuration, err=~p", [Err]),
+    Error
   end.
 
 update_configuration(Conf) ->
@@ -100,6 +86,10 @@ update_configuration(Conf) ->
 
 update_configuration(_, []) ->
   ok;
+update_configuration(Conf, [Key | Rest]) when Key == "aws_greengrass_emqx_auth"; Key == <<"aws_greengrass_emqx_auth">> ->
+  PluginConf = maps:filter(fun(K, _) -> K == Key end, Conf),
+  update_plugin_config(PluginConf),
+  update_configuration(Conf, Rest);
 update_configuration(Conf, [Key | Rest]) ->
   ConfPath = [Key],
   case catch emqx_conf:update(ConfPath, maps:get(Key, Conf), #{rawconf_with_defaults => true, override_to => local}) of
@@ -108,3 +98,25 @@ update_configuration(Conf, [Key | Rest]) ->
     Err -> logger:warning("Failed to update configuration. confPath=~p, error=~p", [ConfPath, Err])
   end,
   update_configuration(Conf, Rest).
+
+update_plugin_config(Conf) ->
+  CheckedConf = validate_plugin_conf(Conf),
+  update_plugin_env(CheckedConf).
+
+validate_plugin_conf(Conf) ->
+  try
+    {_, CheckedConf} = hocon_tconf:map_translate(aws_greengrass_emqx_auth_schema, Conf, #{return_plain => true, format => map}),
+    emqx_map_lib:unsafe_atom_key_map(CheckedConf)
+  catch throw:E:ST ->
+    {error, {config_validation, E, ST}}
+  end.
+
+update_plugin_env(Conf) ->
+  %% TODO find a more dynamic way
+  RootConfig = maps:get(aws_greengrass_emqx_auth, Conf),
+  AuthMode = maps:get(?KEY_AUTH_MODE, RootConfig, ?DEFAULT_AUTH_MODE),
+  application:set_env(?ENV_APP, ?KEY_AUTH_MODE, AuthMode),
+  logger:info("Updated ~p plugin config to ~p", [[?ENV_APP, ?KEY_AUTH_MODE], AuthMode]),
+  UseGreengrassManagedCertificates = maps:get(?KEY_USE_GREENGRASS_MANAGED_CERTIFICATES, RootConfig, ?DEFAULT_USE_GREENGRASS_MANAGED_CERTIFICATES),
+  application:set_env(?ENV_APP, ?KEY_USE_GREENGRASS_MANAGED_CERTIFICATES, UseGreengrassManagedCertificates),
+  logger:info("Updated ~p plugin config to ~p", [[?ENV_APP, ?KEY_USE_GREENGRASS_MANAGED_CERTIFICATES], UseGreengrassManagedCertificates]).
