6588819fcfaa630236ba9236acc3ea624d9a45db
Joseph Cosentino <joecose@amazon.com>
2023-06-19 12:58:07 -0700

    refactor: reorganize

--- gdk-build.py
+++ gdk-build.py
@@ -3,7 +3,6 @@ import os.path
 import subprocess
 
 FORCE = bool(os.getenv('FORCE'))
-DOCKER_ONLY = bool(os.getenv('DOCKER_ONLY'))
 
 COMPONENT_NAME = 'aws.greengrass.clientdevices.mqtt.EMQX'
 COMPONENT_VERSION = 'NEXT_PATCH'
@@ -11,24 +10,23 @@ COMPONENT_VERSION = 'NEXT_PATCH'
 RECIPES_DIR = os.path.join('greengrass-build', 'recipes')
 ARTIFACTS_DIR = os.path.join('greengrass-build', 'artifacts', COMPONENT_NAME, COMPONENT_VERSION)
 
-ZIP_ARTIFACT = os.path.join('build', 'emqx.zip')
-
-TAG = 'aws-greengrass-emqx:amd64'
-IMAGE_FILE = os.path.join(ARTIFACTS_DIR, f'{TAG}.amd64.tar.gz')
-
 
 def build_zip() -> None:
-    if not os.path.exists(ZIP_ARTIFACT) or FORCE:
+    file = os.path.join('build', 'emqx.zip')
+    artifact = os.path.join(ARTIFACTS_DIR, file)
+    if not os.path.exists(artifact) or FORCE:
         import bin
         bin.main()
-    shutil.copy(ZIP_ARTIFACT, ARTIFACTS_DIR)
+        shutil.copy(file, ARTIFACTS_DIR)
 
 
 def build_docker() -> None:
-    if not os.path.exists(IMAGE_FILE) or FORCE:
-        subprocess.check_call(f'docker build . -t {TAG}', shell=True)
+    tag = 'aws-greengrass-emqx:amd64'
+    artifact = os.path.join(ARTIFACTS_DIR, 'aws-greengrass-emqx.amd64.tar.gz')
+    if not os.path.exists(artifact) or FORCE:
+        subprocess.check_call(f'docker build . -t {tag}', shell=True)
         # TODO make more system independent
-        subprocess.check_call(f'docker save {TAG} | gzip > {IMAGE_FILE}', shell=True)
+        subprocess.check_call(f'docker save {tag} | gzip > {artifact}', shell=True)
 
 
 if __name__ == '__main__':
