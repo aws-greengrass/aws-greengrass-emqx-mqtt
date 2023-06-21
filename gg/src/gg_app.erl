03de94673f981598d330dbe4736ab8f5e9f829c7
Joseph Cosentino <joecose@amazon.com>
2023-06-21 16:05:19 -0700

    chore: upgrade to EMQX v5.1.0 (#168)
    * chore: upgrade to 5.1.0-alpha.5
    
    * chore: increase docker build timeout
    
    * chore: upgrade to 5.1.0
    
    * chore: try reverting emqx dep for plugin
    
    * chore: only define emqx.conf values we need, remove dashboard
    
    * chore: fix plugin deps
    
    * chore: consistent formatting
    
    * chore: race condition todo

--- gg/src/gg_app.erl
+++ gg/src/gg_app.erl
@@ -15,8 +15,9 @@ start(_StartType, _StartArgs) ->
   {ok, Sup} = gg_sup:start_link(),
   gg_port_driver:start(),
   gg_conf:start(),
-  enable_cert_verification(),
+  %% TODO race condition, cert loading is async and we need them before restarting ssl listener
   gg_certs:load(),
+  enable_cert_verification(),
   gg:load(application:get_all_env()),
   {ok, Sup}.
 
