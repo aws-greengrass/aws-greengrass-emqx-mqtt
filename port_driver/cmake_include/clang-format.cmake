64bce8b6ac0f6bf1229b3b2b5dee4abf2e0d0664
Joseph Cosentino <joecose@amazon.com>
2024-08-21 12:32:24 -0700

    chore: license headers (#201)

--- port_driver/cmake_include/clang-format.cmake
+++ port_driver/cmake_include/clang-format.cmake
@@ -1,3 +1,7 @@
+# clang-format off
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+# clang-format on
 file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
 
 add_custom_target(
